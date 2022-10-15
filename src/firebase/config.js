0ee14557c532d2e67d29616c7c454787f27433ea
Mihir <mihir0699@gmail.com>
2022-10-15 19:19:00 +0530

    requested changes done

--- src/firebase/config.js
+++
@@ -1,7 +0,0 @@
-import firebase from "firebase/app"
-import "firebase/firestore";
-import {firebaseConfig} from "../firebaseConfig"
-
-  firebase.initializeApp(firebaseConfig);
-
-  export const firestore = firebase.firestore();
